667920aa552e91efa5f478bfc20f37da84622889
septiancahya <cahyaseptian39@gmail.com>
2022-07-13 14:29:19 +0700

    Seeder camp, camp_benefits, admin_user

--- app/Models/Camp.php
+++ app/Models/Camp.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Elequent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Camp extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['title', 'price'];
+    protected $fillable = ['title', 'slug', 'price'];
 }
